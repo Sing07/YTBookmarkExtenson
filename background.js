ae7d76a9c3a10e767781dafc3a86d8aeebe7a47d
SingT <61646543+Sing07@users.noreply.github.com>
2025-04-22 06:47:57 -0700

    repo typo name change test

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
         
         // console.log("6 ejbglawbekfjwknjf,nqoeoqlfe", urlParameters);
 
-        // send custom data/messages to ContentScript.js
+        send custom data/messages to ContentScript.js
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
             videoId: urlParameters.get("v"),
